e72230c24a95bd049217b9f1802fa279a1fb41f3
Anamika-Ratna <anamika008btcsai22@igdtuw.ac.in>
2024-01-04 04:15:24 +0530

    commit

---
+++ portfolio_main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text", {
+      strings: ["Student", "Research Intern", "Open Source Contributor", "Web Developer", "Computer Vision Developer", "Tech Enthusiast"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true
+    });
+  });
+
+
+
+  
